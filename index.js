17518d39eee9cb7e54adadbb9ccc2ff19ce6ec54
Jorge Casal <casaldelacruz@gmail.com>
2020-06-21 11:26:00 +0200

    fix1

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-window.addEventListener('load', getUsers)
-
-function getUsers(){
-    fetch('htps://api.kanye.rest')
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById('quote').innerHTML = `${data.quote}`;
-        document.getElementById('author').innerHTML = '- Mr. West';
-    })
-    .catch(err => {
-        document.getElementById('quote').innerHTML = `${err}`;
-        document.getElementById('author').innerHTML = '- NOT Mr. West';
-    })
-}
+console.log('Jorge Casal')
