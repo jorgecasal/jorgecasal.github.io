b767796b620b1d848fc7dcbe42083fb0157ac695
Jorge Casal <casaldelacruz@gmail.com>
2020-09-15 12:13:07 +0200

    conntact form functionality

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 let theme = localStorage.getItem('theme');
 let themeDots = document.getElementsByClassName('theme-dot');
 
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("contact-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+        form.reset();
+        // button.style = "display: none ";
+        status.classList.add('success')
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.classList.add('error')
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+  // helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+        } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+
 if(theme === null){
     setTheme('light');
 }else{
