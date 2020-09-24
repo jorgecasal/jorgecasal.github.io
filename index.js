18c0b8899cbcaed698158cdb7919bab1cb6d1f38
Jorge Casal <casaldelacruz@gmail.com>
2020-09-24 16:43:41 +0200

    linkedin and navbar

--- index.js
+++ index.js
@@ -85,3 +85,40 @@ function setTheme(mode){
 
     localStorage.setItem('theme', mode)
 }
+
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+
+
+navSlide();
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
