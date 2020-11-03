aef630fb5f2b3c910a779aed1b14db2b22a1e7d2
Jorge Casal <casaldelacruz@gmail.com>
2020-11-03 22:57:38 +0100

    deploy gatsby profil

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -57,7 +57,7 @@ const Blog = () => {
           <Col md={6} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
               <div className="about-wrapper__image">
-                <AboutImg alt="profile picture" filename={'blog.jpg'} />
+                <AboutImg alt="profile picture" filename={'profile.jpg'} />
               </div>
             </Fade>
           </Col>
