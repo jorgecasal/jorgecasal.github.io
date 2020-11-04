59142cf2c79b478cce150f6b36cc645aeffa7e84
Jorge Casal <casaldelacruz@gmail.com>
2020-11-04 12:58:58 +0100

    ready to deploy 1

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,8 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/cdlc/Desktop/github/jorgecasal.github.io/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/cdlc/Desktop/github/jorgecasal.github.io/src/pages/404.js")))
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/cdlc/Desktop/github/jorgecasal.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/cdlc/Desktop/github/jorgecasal.github.io/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/cdlc/Desktop/github/jorgecasal.github.io/src/pages/index.js")))
 }
 
