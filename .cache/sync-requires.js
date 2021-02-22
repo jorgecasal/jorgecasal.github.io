2a50bb3d94dfaadc65ea819542a3711a89df3c41
Jorge Casal <casaldelacruz@gmail.com>
2021-02-22 21:40:01 +0100

    new g

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/cdlc/Desktop/jorgecasal.github.io/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/cdlc/Desktop/jorgecasal.github.io/src/pages/404.js"))),
+  "component---src-pages-element-js": hot(preferDefault(require("/Users/cdlc/Desktop/jorgecasal.github.io/src/pages/Element.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/cdlc/Desktop/jorgecasal.github.io/src/pages/index.js")))
+}
+
