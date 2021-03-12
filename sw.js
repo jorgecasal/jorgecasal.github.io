c4860d5541b455f457e8bbe196c25337775e7195
Jorge Casal <casaldelacruz@gmail.com>
2021-03-12 13:32:36 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-39f8f1b16c141b11516f.js"
+    "url": "webpack-runtime-da4c6e9abe469f1babea.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "320a7b56b7c400ca99a128bc690d1c53"
+    "revision": "9db4ee3c7c10285bde268253b4113739"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
